4b52dca8bee186c3e7fbc68c7fbbda8c32f3a64b
Jeremiah Senkpiel <fishrock123@rocketmail.com>
2020-10-17 10:59:29 -0700

    src: set rustc lint levels

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+#![forbid(unsafe_code, future_incompatible)]
+#![warn(
+    missing_debug_implementations,
+    rust_2018_idioms,
+    trivial_casts,
+    unused_qualifications
+)]
+#![doc(test(attr(deny(rust_2018_idioms, warnings))))]
+#![doc(test(attr(allow(unused_extern_crates, unused_variables))))]
+
 //! Outgoing HTTP body compression middleware for Tide.
 
 mod middleware;
