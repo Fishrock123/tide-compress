3939cae2cbb51a3866007e5fe92f9185b4978dbb
Jeremiah Senkpiel <fishrock123@rocketmail.com>
2022-07-04 11:58:39 -0700

    doc: fix doc comment list indentation

--- src/lib.rs
+++ src/lib.rs
@@ -21,19 +21,19 @@
 //! ## Features
 //!
 //! - Support for [Brotli][], [Gzip][], and [Deflate][] encodings, compile-time configurable through cargo feature flags.
-//! - Prioritizes Brotli if available.
-//! - Only pulls in the necessary dependencies for the desired configuration.
-//! - Defaults to Brotli & Gzip.
+//!   - Prioritizes Brotli if available.
+//!   - Only pulls in the necessary dependencies for the desired configuration.
+//!   - Defaults to Brotli & Gzip.
 //! - [`Accept-Encoding`][] header checking including priority.
 //! - Minimum body size threshold (Default: 1024 bytes, configurable).
 //! - Does not compress responses with a [`Cache-Control: no-transform`][] header.
 //! - Sets the [`Vary`][] header.
 //! - Checks the [`Content-Type`][] header (MIME).
-//! - Checks against [jshttp's comprehensive database][jshttp mime-db], which is compiled to a [perfect hash function][] at build time.
-//! - If not in the database, checks against a regular expression.
+//!   - Checks against [jshttp's comprehensive database][jshttp mime-db], which is compiled to a [perfect hash function][] at build time.
+//!   - If not in the database, checks against a regular expression.
 //!     - Default: `^text/|\+(?:json|text|xml)$` (case insensitive).
 //!     - Fully override-able to any custom [`Regex`][], with `None` as an option.
-//! - Functionality can be excluded in crate features if the `regex` crate or codegen poses build issues.
+//!   - Functionality can be excluded in crate features if the `regex` crate or codegen poses build issues.
 //!
 //! ## Note
 //!
