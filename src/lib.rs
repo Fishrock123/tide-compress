ea314262b514ce38abcae9c8b5073ccd735d3008
Jeremiah Senkpiel <fishrock123@rocketmail.com>
2022-07-31 15:21:23 -0700

    docs: no longer generates at build time

--- src/lib.rs
+++ src/lib.rs
@@ -71,16 +71,12 @@
 //! - Does not compress responses with a [`Cache-Control: no-transform`][] header.
 //! - Sets the [`Vary`][] header.
 //! - Checks the [`Content-Type`][] header (MIME).
-//!   - Checks against [jshttp's comprehensive database][jshttp mime-db], which is compiled to a [perfect hash function][] at build time.
+//!   - Checks against [jshttp's comprehensive database][jshttp mime-db], which is compiled to a [perfect hash function][].
+//!   - The database can be regenerated in the crate's repository by running `cargo run generate-database`.
 //!   - If not in the database, checks against a regular expression.
 //!     - Default: `^text/|\+(?:json|text|xml)$` (case insensitive).
 //!     - Fully override-able to any custom [`Regex`][], with `None` as an option.
-//!   - Functionality can be excluded in crate features if the `regex` crate or codegen poses build issues.
-//!
-//! ## Note
-//!
-//! This crate, in its current set up with the `db-check` feature enabled (which is by default),
-//! pulls down [a json MIME database][jshttp mime-db] from the network at build time.
+//!   - Functionality can be excluded in crate features if the `regex` crate poses build issues.
 //!
 //! [`Accept-Encoding`]: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Accept-Encoding
 //! [`Cache-Control: no-transform`]: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Cache-Control
